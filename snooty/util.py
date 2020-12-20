6858ccc4bd42ea3b47976fc05636f8f4d388209c
Lex Li <support@lextm.com>
2020-12-19 23:40:56 -0500

    Returned original path when not found.

--- snooty/util.py
+++ snooty/util.py
@@ -109,17 +109,15 @@ def add_doc_target_ext(target: str, docpath: PurePath, project_root: Path) -> Pa
     if os.path.exists(resolved_target_path):
         return resolved_target_path
     # Adding the current suffix first takes into account dotted targets
-    last: Path = Path()
     for ext in RST_EXTENSIONS:
         new_suffix = target_path.suffix + ext
         temp_path = target_path.with_suffix(new_suffix)
 
         fileid, resolved_target_path = reroot_path(temp_path, docpath, project_root)
-        last = resolved_target_path
         if os.path.exists(resolved_target_path):
             return resolved_target_path
-    # If none of the files exists, return the last attempted file path to trigger errors.
-    return last
+    # If none of the files exists, return the original file path to trigger errors.
+    return resolved_target_path
 
 
 class FileWatcher:
